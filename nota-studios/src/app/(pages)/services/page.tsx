171912cbe25322b41c435913a75c28bf45ae8521
Lyndon Shelton <Lyndon.dshelton@gmail.com>
2024-12-29 09:25:28 -0500

    Initial content for Services
    Initialize the services page.

--- nota-studios/src/app/(pages)/services/page.tsx
+++ nota-studios/src/app/(pages)/services/page.tsx
@@ -1,9 +1,42 @@
-
+import ServicesList from "@/app/components/services-accordian-list"
 
 export default function Services(){
+    const studioHours = [
+        {day:"Monday", hours:"4:00PM - 12:00AM"},
+        {day:"Tuesday", hours:"4:00PM - 12:00AM"},
+        {day:"Wednesday", hours:"4:00PM - 12:00AM"},
+        {day:"Thursday", hours:"4:00PM - 12:00AM"},
+        {day:"Friday", hours:"4:00PM - 12:00AM"},
+        {day:"Saturday", hours:"4:00PM - 12:00AM"},
+        {day:"Sunday", hours:"4:00PM - 12:00AM"},
+    ]
+    const studioEquipment =[
+        "FL Studios + Pro Tools + Garage Band",
+        'KRK CLASSIC 5" MONITORS',
+        "FOCUSRITE CM25 MKIII CONDENSER MICROPHONE",
+        "SSL 2+ AUDIO INTERFACE",
+        "ALESIS V25 MIDI KEYBOARD"
+    ]
     return(
         <>
-        
+        <div className="flex md:flex-row sm:flex-col">
+            <div className="md:w-1/2 sm:w-full p-6 md:order-2" >
+                <ServicesList />
+            </div>
+            <div className="md:w-1/2 sm:w-full p-6 md:order-first">
+                <h3>Studio Hours</h3>
+                {/* print each studio hours */}
+                {studioHours.map((schedule, index) =>(
+                    <p key={index}>{schedule.day} {schedule.hours}</p>
+                ))}
+
+                <h3 className="mt-4">Studio Equipment</h3>
+                {/* print each studio equipment*/}
+                {studioEquipment.map((equipment, index) => (
+                    <p key={index}>{equipment}</p>
+                ))}
+            </div>
+        </div>
         </>
     )
 }
--- nota-studios/src/app/components/accordian/accordian-list-item.tsx
+++ nota-studios/src/app/components/accordian/accordian-list-item.tsx
@@ -1,10 +1,19 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+'use client';
+import Image from "next/image";
 
-
-export default function AccordianItem(){
+export default function AccordianItem(props){
     return(
         <>
-
+            <h4 className="border-t-2 last:border-b-2 p-4">
+                <button className="flex items-center w-full justify-between">
+                    <span className="flex-initial">{props.serviceName}</span>
+                    <div className="w-6 h-6 flex-initial">
+                        <div className="plus">
+                            <Image src="/plus-svgrepo-com.svg" alt="plus-svgrepo-com.svg" width={800} height={800} className="w-full"/>
+                        </div>
+                    </div>
+                </button>
+            </h4>
         </>
     );
 }
