253069b33054a05d3a0aeac05a9b940ca5f1fd4d
Lyndon Shelton <Lyndon.dshelton@gmail.com>
2025-01-13 20:34:45 -0500

    Update layout.tsx
    -add margin auto to the body tag to center content on extra-large screens.

--- nota-studios/src/app/layout.tsx
+++ nota-studios/src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-screen-2xl`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-screen-2xl m-auto`}
       >
         <Header />
 
