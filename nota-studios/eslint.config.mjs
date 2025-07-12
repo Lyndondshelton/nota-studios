d0454df23facf205bf34bb8eb1536240602bc0f9
Lyndon Shelton <Lyndon.dshelton@gmail.com>
2025-07-11 22:03:12 -0400

    B-000013 - update ESLint rules

--- nota-studios/eslint.config.mjs
+++ nota-studios/eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    rules:{
+      "no-unused-vars":["error",{args:"none"}],
+      "@typescript-eslint/no-unused-vars": ["error", { args: "none" }],
+    }
+  }
 ];
 
 export default eslintConfig;
